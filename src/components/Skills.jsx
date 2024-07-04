0f0f55409f5eec3ee12b6ef035382d500fcda1f4
basalamahalam <basalamahalam@upi.edu>
2024-07-04 20:23:43 +0700

    kasih card

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,9 +7,9 @@ import { FaLaravel } from "react-icons/fa";
 import { DiCodeigniter } from "react-icons/di";
 import { RiNextjsFill } from "react-icons/ri";
 import { SiFlutter } from "react-icons/si";
-import { BiLogoCPlusPlus } from "react-icons/bi";
 import { FaBootstrap } from "react-icons/fa";
 import { FiFigma } from "react-icons/fi";
+import { IoLogoGithub } from "react-icons/io";
 
 const Skills = () => {
   return (
@@ -17,13 +17,86 @@ const Skills = () => {
       <h1 className="text-white font-lato font-bold text-2xl tracking-wider">
         My Skills
       </h1>
-      <div className="flex items-center justify-center">
-        <FaHtml5 className="text-white text-4xl" />
-        <IoLogoCss3 className="text-white text-4xl" />
-        <IoLogoJavascript className="text-white text-4xl" />
-        <FaReact className="text-white text-4xl" />
+      <div className="py-10">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center justify-start w-[200px]">
+            <FaHtml5 className="text-white text-4xl mr-5" />
+            <h1 className="font-lato font-medium text-xl text-white tracking-wider">
+              HTML
+            </h1>
+          </div>
+          <div className="flex items-center justify-start w-[200px]">
+            <IoLogoCss3 className="text-white text-4xl mr-5" />
+            <h1 className="font-lato font-medium text-xl text-white tracking-wider">
+              CSS
+            </h1>
+          </div>
+          <div className="flex items-center justify-start w-[200px]">
+            <IoLogoJavascript className="text-white text-4xl mr-5" />
+            <h1 className="font-lato font-medium text-xl text-white tracking-wider">
+              Javascript
+            </h1>
+          </div>
+          <div className="flex items-center justify-start w-[200px]">
+            <FaReact className="text-white text-4xl mr-5" />
+            <h1 className="font-lato font-medium text-xl text-white tracking-wider">
+              ReactJs
+            </h1>
+          </div>
+        </div>
+        <div className="flex items-center justify-between mt-10">
+          <div className="flex items-center justify-start w-[200px]">
+            <RiNextjsFill className="text-white text-4xl mr-5" />
+            <h1 className="text-white font-medium font-lato text-xl tracking-wider">
+              NextJs
+            </h1>
+          </div>
+          <div className="flex items-center justify-start w-[200px]">
+            <FaLaravel className="text-white text-4xl mr-5" />
+            <h1 className="font-lato font-medium text-xl text-white tracking-wider">
+              Laravel
+            </h1>
+          </div>
+          <div className="flex items-center justify-start w-[200px]">
+            <DiCodeigniter className="text-white text-4xl mr-5" />
+            <h1 className="font-lato font-medium text-xl text-white tracking-wider">
+              Codeigniter
+            </h1>
+          </div>
+          <div className="flex items-center justify-start w-[200px]">
+            <SiFlutter className="text-white text-4xl mr-5" />
+            <h1 className="font-lato font-medium text-xl tracking-wider text-white">
+              Flutter
+            </h1>
+          </div>
+        </div>
+        <div className="flex items-center justify-between mt-10">
+          <div className="flex items-center justify-start w-[200px]">
+            <RiTailwindCssFill className="text-white text-4xl mr-5" />
+            <h1 className="font-lato font-medium text-xl text-white tracking-wider">
+              TailwindCSS
+            </h1>
+          </div>
+          <div className="flex items-center justify-start w-[200px]">
+            <FaBootstrap className="text-white text-4xl mr-5" />
+            <h1 className="font-lato font-medium text-xl text-white tracking-wider">
+              Bootstrap
+            </h1>
+          </div>
+          <div className="flex items-center justify-start w-[200px]">
+            <FiFigma className="text-white text-4xl mr-5" />
+            <h1 className="font-lato font-medium text-xl text-white tracking-wider">
+              Figma
+            </h1>
+          </div>
+          <div className="flex items-center justify-start w-[200px]">
+            <IoLogoGithub className="text-white text-4xl mr-5" />
+            <h1 className="font-lato font-medium text-xl text-white tracking-wider">
+              Github
+            </h1>
+          </div>
+        </div>
       </div>
-      <RiNextjsFill className="text-white text-4xl" />
     </div>
   );
 };
